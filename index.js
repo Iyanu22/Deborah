88db31ad2547b57d99a8fb00e05dd3a4cab8b57f
Iyanu22 <55497539+Iyanu22@users.noreply.github.com>
2019-09-18 15:32:28 +0100

    HTML, CSS, Javascript code
    Using react.js to create a blog listing application that lists posts from WordPress blog API. 
     The code extracts posts from the wordpress blog, lists the title and when you click the title, it loads to show the full content of the post you click

---
+++ index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      posts: [],
+      post: null,
+      singleView: false
+    };
+  }
+
+  viewPost = (post, e) => {
+    this.setState({
+      post: post,
+      singleView: true
+    });
+  }
+
+  viewPosts = (e) => {
+    this.setState({
+      post: null,
+      singleView: false
+    });
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8888/kadmieljames/wp-json/wp/v2/posts?_embed")
+      .then(res => res.json())
+      .then(
+        (posts) => {
+          this.setState({
+            isLoaded: true,
+            posts: posts
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, posts, post, singleView } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else if (singleView) {
+      return (
+        <div>
+
+{post.featured_media ?
+							<a href={post.link}><img alt="" src={post._embedded['wp:featuredmedia'][0].source_url} /></a>
+						: null}
+						
+						<button onClick={this.viewPosts}>Home</button>
+          <div className="post">
+            <h1>{post.title.rendered}</h1>
+            <span>{post.date}</span>
+            <div dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="posts">
+          <h1>Blog</h1>
+          <ul>
+            {posts.map(post => (
+              <li key={post.id}>
+                <h2 onClick={(e) => this.viewPost(post, e)}>{post.title.rendered}</h2>
+                <span>{post.date}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
